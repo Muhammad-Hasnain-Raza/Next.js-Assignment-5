2fac7dc87e82f56b37c23ad14d34ecc1acf3a13c
Muhammad Hasnain Raza <hassnainrazam009@gmail.com>
2024-11-10 22:33:25 +0500

    completetd

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function header() {
+  return (
+    <header className="bg-[#A29875] w-[full] h-[65px] text-white font-[Rye] ">
+      <h1 className="text-5xl pt-2 pl-4">MANZARRI</h1>
+    </header>
+  )
+}
+
+export default header
